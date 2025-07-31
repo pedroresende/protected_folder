94c4459a6925a6fe0575fa6174cd4f7cc36c7d27
Pedro Resende <pedroresende@users.noreply.github.com>
2025-07-31 19:32:34 +0100

    fix: failing test:system

--- test/controllers/protected_folder_controller_test.rb
+++ test/controllers/protected_folder_controller_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class ProtectedFolderControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     get protected_folder_show_url
-    assert_response :success
+    assert_response :unauthorized
   end
 end
